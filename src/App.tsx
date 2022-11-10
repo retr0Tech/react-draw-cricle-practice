d1530b2a76007e4f2cfd3ac0714d5b45f2023448
elliot.ruiz <ruiz@transformait.io>
2022-11-10 11:24:03 -0500

    draw circle in grid practice for react interviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isParameterPropertyDeclaration } from 'typescript';
+
+type TPoint = {
+	x: number,
+	y: number
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [points, setPoints] = useState<TPoint[]>([]);
+	const [popped, setPopped] = useState<TPoint[]>([]);
+
+	function placeCircle(e: React.MouseEvent<HTMLDivElement>){
+		const {clientX, clientY} = e;
+		setPoints([...points, {x:clientX, y:clientY,}]);
+	}
+	function undo(){
+		const newPoints = [...points];
+		const poppedPoint = newPoints.pop();
+		if(!poppedPoint) return;
+		setPopped([...popped, poppedPoint]);
+		setPoints(newPoints);
+	}
+
+	function redo(){
+		const newPopped = [...popped];
+		const poppedPoint = newPopped.pop();
+		if(!poppedPoint) return;
+		setPoints([...points, poppedPoint]);
+		setPopped(newPopped);
+
+	}
+
+
+	return (
+		<>
+			<button disabled={points.length === 0} onClick={undo}>Undo</button>
+			<button disabled={popped.length === 0} onClick={redo}>Redo</button>
+			<div className="App" onClick={placeCircle}>
+				{points.map((point, index) =>(
+					<div
+					key={index}
+					className="circle"
+					style={{
+						left: point.x - 10 + "px",
+						top: point.y - 10 + "px",
+					}}></div>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default App;
